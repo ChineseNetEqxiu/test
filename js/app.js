12e3f66fc5d5a129dbd25557d1d6de0f3f794908
wangshijiang <wangshijiang@eqxiu.com>
2016-04-25 13:39:31 +0800

    页面大体布局

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 angular.module('test', [])
-    .controller('QuestionListCtrl', ['$scope', function ($scope) {
-
+    .controller('QuestionListCtrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.keywords = '';
+        $http.get('assets/qa.json')
+            .then(function (res) {
+                $scope.questions = res.data;
+            });
     }]);
