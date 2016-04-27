400b64a8e980766032fd20c614e0157872d9229b
wangshijiang <wangshijiang@eqxiu.com>
2016-04-27 10:48:09 +0800

    添加超人模式

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 angular.module('test', ['questions', 'completions', 'ngSanitize'])
-    .controller('QuestionListCtrl', ['$scope', '$http', 'questions', 'completions', function ($scope, $http, questions, completions) {
+    .controller('QuestionListCtrl', ['$scope', '$http', 'questions', 'completions', '$compile', function ($scope, $http, questions, completions, $compile) {
         $scope.keywords = {title: ''};
         //$http.get('assets/qa.json')
         //    .then(function (res) {
@@ -8,6 +8,21 @@ angular.module('test', ['questions', 'completions', 'ngSanitize'])
         $scope.questions = questions;
         $scope.completions = completions;
         $scope.answerOnly = false;
+
+        Mousetrap.bind('s u p e r m a n', enableSupermanMode);
+        function enableSupermanMode() {
+            var $keywords = $('#keywords');
+            $keywords.attr('ng-model', 'keywords.title')
+                .attr('placeholder', '点击输入框，或键入\'\/\'，录入关键字（超人模式开启）')
+                .removeAttr('readonly');
+            $compile($keywords)($scope);
+            setTimeout(function () {
+                $keywords.focus().select();
+            }, 0);
+        }
+        $scope.$on('$destroy', function () {
+            Mousetrap.unbind('superman', enableSupermanMode);
+        });
     }])
     .directive('bindSlash', function () {
         return function ($scope, $element) {
